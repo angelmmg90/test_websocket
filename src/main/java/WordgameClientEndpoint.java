24ec8456bb454d5716fabef04e526d755b91bb6e
Ángel <amacdong@usersad.everis.int>
2017-08-02 11:52:55 +0200

    Created onMessage method in WebSocket Client Endpint. The method  annotated with @OnMessage is called each time a message is received from server

--- src/main/java/WordgameClientEndpoint.java
+++ src/main/java/WordgameClientEndpoint.java
@@ -1,12 +1,16 @@
 import javax.websocket.ClientEndpoint;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
 @ClientEndpoint
 public class WordgameClientEndpoint {
     private Logger logger = Logger.getLogger(this.getClass().getName());
+
 
     @OnOpen
     public void onOpen(Session session){
@@ -18,6 +22,18 @@ public class WordgameClientEndpoint {
             throw new RuntimeException(e);
         }
 
+    }
+
+    @OnMessage
+    public String onMessage(String message, Session session){
+        BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            logger.info("Received ..." + message);
+            String userInput = bufferRead.readLine();
+            return userInput;
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
     }
 
 }
