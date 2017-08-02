0b48f8d5bbb15494798790fa54ac5290b080e7c7
Ángel <amacdong@usersad.everis.int>
2017-08-02 11:32:07 +0200

    Created mehtod onMessage. The @OnMessage annotation is used to annotate a method which will be called each time a message is received. This is the method where all the business code will be written. If we receive quit message we call the close method on session object giving it the reason for clossing the session.

--- src/main/java/WordgameServerEndpoint.java
+++ src/main/java/WordgameServerEndpoint.java
@@ -1,6 +1,9 @@
+import javax.websocket.CloseReason;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 @ServerEndpoint(value = "/game")
@@ -10,6 +13,23 @@ public class WordgameServerEndpoint {
     @OnOpen
     public void onOpen(Session session){
         logger.info("Connected ..." + session.getId());
+    }
+
+    @OnMessage
+    public String onMessage(String message, Session session){
+        switch(message){
+            case "quit":
+                try {
+                    session.close(
+                            new CloseReason(CloseReason.CloseCodes.NORMAL_CLOSURE,
+                                    "Game ended"));
+
+                }catch (IOException e){
+                    throw new RuntimeException(e);
+                }
+                break;
+        }
+        return message;
     }
 
 
