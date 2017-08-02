0dec539700257b6cc7595a0521616c71f45912a0
Ángel <amacdong@usersad.everis.int>
2017-08-02 12:22:18 +0200

    Created a maint method which waits till the time latch decrements the counter in onClose() method. In the main method we create instance of ClientManager which is then used to connect to Server Endpoint

--- src/main/java/WordgameClientEndpoint.java
+++ src/main/java/WordgameClientEndpoint.java
@@ -1,11 +1,18 @@
+import org.glassfish.tyrus.client.ClientManager;
+
 import javax.websocket.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.CountDownLatch;
 import java.util.logging.Logger;
 
 @ClientEndpoint
 public class WordgameClientEndpoint {
+
+    private static CountDownLatch latch;
     private Logger logger = Logger.getLogger(this.getClass().getName());
 
 
@@ -39,8 +46,26 @@ public class WordgameClientEndpoint {
     tion is closed.*/
     @OnClose
     public void onClose(Session session, CloseReason closeReason){
-        logger.info(String.format("Session %s close because of %s", session.getId()));
+        logger.info(String.format("Session %s close because of %s", session.getId(), closeReason));
+        latch.countDown();
 
+
+    }
+
+    /*In the main method we create instance of ClientManager which
+    is then used to connect to Server Endpoint*/
+    public static void main(String[] args){
+        latch = new CountDownLatch(1);
+
+        ClientManager client = ClientManager.createClient();
+        try{
+            client.connectToServer(WordgameClientEndpoint.class,
+                    new URI("ws://localhost:8025/websockets/game"));
+            latch.await();
+
+        }catch (DeploymentException | URISyntaxException | InterruptedException e){
+            throw new RuntimeException(e);
+        }
     }
 
 }
